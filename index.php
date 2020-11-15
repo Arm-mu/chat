507cd240658ddfc9dc708cf54898cd446e3eed6d
FoxLyz <38246160+FoxLyz@users.noreply.github.com>
2020-11-15 23:58:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('http://localhost/Facebookuserprofile/fb-profile.php', $permissions);
 //echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 header("location:" . $loginUrl);
-echo $loginUrl;
+//echo $loginUrl;
 ?>
