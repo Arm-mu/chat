11e468b64192d357322ed4af7f1a5b32822ad85e
FoxLyz <38246160+FoxLyz@users.noreply.github.com>
2020-11-15 23:57:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('http://localhost/Facebookuserprofile/fb-profile.php', $permissions);
 //echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-//header("location:" . $loginUrl);
+header("location:" . $loginUrl);
 echo $loginUrl;
 ?>
