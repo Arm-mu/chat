d5f17d599552ec97d95115830a2b68ae2587bfa0
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2020-11-23 02:07:05 +0700

    change name
    index to login

--- login.php
+++ login.php
@@ -5,7 +5,5 @@ require 'config.php';
 $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('https://github.com/FoxLyz/chat/blob/master/fb-profile.php', $permissions);
-//echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 header("location:" . $loginUrl);
-//echo $loginUrl;
 ?>
