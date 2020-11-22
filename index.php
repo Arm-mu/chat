984d2ff5b2a7f01622ec3a51beb9ffbe263e2d0e
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2020-11-23 02:08:26 +0700

    update a lot
    change name,change code

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <meta charset="utf-8">
 <?php
 
+require 'login.php';
+
 session_start();
+
 require_once __DIR__ . '/Facebook/autoload.php';
 
-$fb = new Facebook\Facebook([
-    'app_id' => '1513458998840910',
-    'app_secret' => '20c5684ffb4358ad7436279464a691fd', 
-    'default_graph_version' => 'v2.4',
-  ]);
+require 'config.php';
 
 $helper = $fb->getRedirectLoginHelper();
  
