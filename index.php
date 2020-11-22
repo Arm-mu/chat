8d26d1c1cbdc7100392645e11e85886d497635d6
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2020-11-23 01:10:17 +0700

    UpdateURL
    UpdateURL

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'config.php';
 
 $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://github.com/FoxLyz/chat/blob/master/fb-profile.php', $permissions);
+$loginUrl = $helper->getLoginUrl('https://github.com/FoxLyz/chat/blob/master/fb-profile.php', $permissions);
 //echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 header("location:" . $loginUrl);
 //echo $loginUrl;
